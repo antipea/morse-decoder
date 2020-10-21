fad9ca09f173d890cb89b3474e352a6d6f4b491e
antipea <71250351+antipea@users.noreply.github.com>
2020-10-21 13:24:41 +0300

    feat: add common function decode()

--- src/index.js
+++ src/index.js
@@ -39,6 +39,27 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    let decodedExpr = '';
+    let newExpr, newExprTwo, newLetter;
+
+    expr = expr.split('**********');
+    for (let i = 0; i < expr.length; i++) {
+        newExpr = expr[i].match(/.{1,10}/g);
+        let word = '';
+        for (let j = 0; j < newExpr.length; j++) {
+            newExprTwo = newExpr[j].match(/.{1,2}/g)/*.reverse()*/;
+            let codeLetter = '';
+            for (let y = 0; y < newExprTwo.length; y++) {
+                if (newExprTwo[y] === '10') codeLetter  = `${codeLetter}.`;
+                if (newExprTwo[y] === '11') codeLetter  = `${codeLetter}-`;
+            }
+            newLetter = MORSE_TABLE[codeLetter];
+            word = `${word}${newLetter}`;
+        }
+        decodedExpr = `${decodedExpr}${word} `;
+    }
+
+    return decodedExpr.substring(0, decodedExpr.length - 1);
 }
 
 module.exports = {
